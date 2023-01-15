d3ca056536c2be50b4363f32855812422baa65c4
Fernando <fernando.pr71189@gmail.com>
2023-01-15 15:44:17 -0600

    clase book

---
+++ PaginationMVC/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PaginationMVC.Models
+{
+    public class Book
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private int PubDate { get; set; }
+        public Book(string Title, string Director, int PubDate)
+        {
+            this.Title = Title;
+            this.Director = Director;
+            this.PubDate = PubDate;
+        }
+    }
+}
